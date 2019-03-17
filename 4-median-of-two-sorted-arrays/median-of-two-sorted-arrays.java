5443c1a5ec5eb8daaab370d19eb7a4dc29fd048a
chenjr15 <icjr@qq.com>
2019-03-17 20:55:39 +0800

    :heavy_check_mark:
    Runtime: 23 ms, faster than 99.17% of Java online submissions for Median of Two Sorted Arrays.
    Memory Usage: 48.7 MB, less than 68.03% of Java online submissions for Median of Two Sorted Arrays.

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,71 +1,58 @@
 class Solution {
+    private int idx1=0,idx2=0;
+    private int[] nums1=null;
+    private int[] nums2 = null;
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
         int total_size = nums1.length +nums2.length;
         int center =(total_size/2); 
-        boolean odd = true;
-        int[] selected = null;
         double median=0;
-        int x=0,y=0;
-        for(int i =0;i< center;i++){
-            if(x<nums1.length && y<nums2.length){
-                if(nums1[x]<nums2[y]){
-                    x++;
-                    selected=nums1;
-                }else{
-                    y++;
-                    selected=nums2;
-                }
-            }else{
-                if(x<nums1.length){
-                    x++;
-                    selected=nums1;
-
-                }else{
-                    y++;
-                    selected=nums2;
-
-                }
-            }
+        if((total_size - 2*center)==0){
+            // 少一次
+            median = this.fastfoward(center-1);
+            median = (median+fastfoward(1))/2;
+        }else{
+            // center次
+            median = fastfoward(center);
         }
-        // median = selected[((selected == nums1)?x:y)-1];
-        if(x<nums1.length && y<nums2.length){
-                if(nums1[x]<nums2[y]){
-                    median += nums1[x];
-                }else{
-                    median += nums2[y];
-                }
-            }else{
-                if(x<nums1.length){
-                    median += nums1[x];
-
-                }else{
-                   median += nums2[y];
-
-                }
-            }
-        int next;
-        if(2*center == total_size){
-             if(x<nums1.length && y<nums2.length){
-                if(nums1[x]<nums2[y]){
-                    median += nums1[x];
-                }else{
-                    median += nums2[y];
-                }
-            }else{
-                if(x<nums1.length){
-                    median += nums1[x];
-
-                }else{
-                   median += nums2[y];
-
-                }
-            }
-            median /=2;
-        }
+        
         
         return median;
     }
-    public static final int  smaller(int[] nums1, int[] nums2,Interger idx1,Interger idx2){
-        
+    public int  fastfoward(int count){
+        int len1=nums1.length;
+        int len2 = nums2.length;
+        while((count--) != 0){
+            if(idx1<len1&& idx2<len2){
+                if(nums1[idx1]<nums2[idx2]){
+                    idx1++;
+                }else{
+                    idx2++;
+                }
+            }else{
+                if (idx1<len1){
+                    idx1++;
+                }else{
+                    idx2++;
+                }
+            }
+        }
+        int smaller = 0;
+        // 返回快进指针指向的比较小的元素
+        if(idx1<len1&& idx2<len2){
+                if(nums1[idx1]<nums2[idx2]){
+                    smaller = nums1[idx1]; 
+                }else{
+                    smaller = nums2[idx2]; 
+                }
+            }else{
+                if (idx1<len1){
+                    smaller = nums1[idx1]; 
+                }else{
+                    smaller = nums2[idx2]; 
+                }
+            }
+        return smaller;
     }
 }
