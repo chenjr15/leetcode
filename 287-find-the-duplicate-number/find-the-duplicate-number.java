45783b39002afe21ce8aa5554cef8701120434bd
chenjr15 <icjr@qq.com>
2019-03-18 20:38:36 +0800

    采用了环检测算法

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,12 +1,29 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            for(int j = i+1;j<nums.length;j++){
-                if(nums[i] == nums[j]){
-                    return nums[i];
-                }
-            }
+        int fast =nums[0];
+        int slow = nums[0];
+        // 走两步
+            fast=nums[fast];
+            fast=nums[fast];
+            // 走一步
+            slow=nums[slow];
+        while(fast!=slow){
+            // 走两步
+            fast=nums[fast];
+            fast=nums[fast];
+            // 走一步
+            slow=nums[slow];
         }
-        return -1;
+        
+        fast = nums[0];
+        while(fast!=slow){
+            // 走一步
+            fast=nums[fast];
+            // 走一步
+            slow=nums[slow];
+            
+        }
+        return fast;
+        
     }
 }
