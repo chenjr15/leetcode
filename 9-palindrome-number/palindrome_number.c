16f80747cec69a40e0094a3550eb05da8248b867
chenjr15 <icjr@qq.com>
2019-03-23 14:03:19 +0800

    参考了答案学习了反转一半数字的方法

--- 9-palindrome-number/palindrome_number.c
+++ 9-palindrome-number/palindrome_number.c
@@ -1,26 +1,22 @@
+// 采用反转一半的思想
 bool isPalindrome(int x) {
+    // 去除负数, 因为负号的存在所以不会成为回文数
     if(x<0){return false;}
-    if(x == 0){return true;}
-    // 这个大概也是常数级的空间吧（大雾）
-    char buffer[11] = {0};
-    char* p = buffer;
-    while(x){
-        *p++ = x%10;
+    // 对于尾数为0的数, 其最高位应为0(其实只有0符合), 故去除最低位为0, 且不等于0的数
+    if((x%10 == 0 ) &&( x !=0 )){return false;}
+    int reversed = 0;
+    while(x>reversed){
+        reversed *=10;
+        reversed += x%10;
         x/=10;
     }
-    
-    int i,j;
-    i=0;j=p-buffer-1;
-    p = buffer;
-    while(j>i){
-        if(*(p+i) != *(p+j)){
-            break;
-        }
-        i++;j--;
-    }
-    if(i >= j){
+    if(reversed ==  x){
         return true;
+    }else if (reversed > x){
+        // 这里判断是否为位数为奇数的回文数(中间单独一个数字, eg. 121). 
+        // 对于这样的数, 中间的那位数字会加在反转后的那部分数字上, 因此去除最低位后比较
+        return x == (reversed/10);
     }else{
-        return false;half
+        return false;
     }
 }
