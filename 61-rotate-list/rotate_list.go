603648c083a915ebd6179f0e2fb2bf9b7696e1e3
chenjr <chenjr@live.cn>
2019-07-31 08:59:05 +0800

    用另一个指针优化了一次遍历

--- 61-rotate-list/rotate_list.go
+++ 61-rotate-list/rotate_list.go
@@ -7,29 +7,28 @@
  */
 // rotateRight Linking the linked list into a ring, then you can rotate it.
 func rotateRight(head *ListNode, k int) *ListNode {
-    if head == nil {
-        return head
-    }
-    length := 1
-    
-    // 找尾
-    p := head
-    for ;p.Next!=nil;p = p.Next{
-        length++
-    }
-    // 接环
-    p.Next = head
-    // 旋转
-    k %= length
-    k = length-k
-    for p= head;k>0;k--{
-        head = head.Next
-    }
-    // 断环
-    for p = head;p.Next != head ;p = p.Next {
-    }
-    p.Next = nil
-    
-    return head
-    
+	if head == nil {
+		return head
+	}
+	length := 1
+
+	// 找尾
+	p := head
+	for ; p.Next != nil; p = p.Next {
+		length++
+	}
+	// 接环
+	p.Next = head
+	// 旋转, 这里p一直指向head的前一个
+	k %= length
+	k = length - k
+	for ; k > 0; k-- {
+		p = head
+		head = head.Next
+	}
+	// 断环
+	p.Next = nil
+
+	return head
+
 }
