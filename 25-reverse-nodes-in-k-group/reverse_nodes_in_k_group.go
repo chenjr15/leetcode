0c4c81bbb796bcba09f8efc8666df26186d5b988
chenjr15 <chenjr15@live.cn>
2019-08-28 09:06:07 +0800

    同24，分组反转， 加了一个长度判断

---
+++ 25-reverse-nodes-in-k-group/reverse_nodes_in_k_group.go
@@ -0,0 +1,56 @@
+package leetcode25
+
+import "github.com/chenjr15/golist/linkedlist"
+
+type ListNode = linkedlist.ListNode
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+
+	if head == nil || head.Next == nil || k == 0 {
+		return head
+	}
+
+	newHead := reverseN(head, k)
+	pre := head
+	p := head.Next
+	for p != nil && p.Next != nil {
+		// 保证前面的指向新的头
+		pre.Next = reverseN(p, k)
+		pre = p
+		p = p.Next
+	}
+	return newHead
+
+}
+
+// 部分逆序，reverseLength 指定要逆序的长度，然会新的头节点
+func reverseN(head *ListNode, reverseLength int) (newhead *ListNode) {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	p := head
+	length := 0
+	for p != nil {
+		p = p.Next
+		length++
+	}
+	// 加一波长度判定，对最后长度小于反转长度的那组忽略不转
+	if length < reverseLength {
+		return head
+	}
+	p = head
+	var next, pre *ListNode
+	// 逐个反转
+	for reverseLength > 0 && p != nil {
+		next = p.Next
+		p.Next = pre
+		pre = p
+		p = next
+		reverseLength--
+	}
+
+	head.Next = p
+	return pre
+
+}
