18d6b48a1b77f066b51cc68cd58f4e7355cda134
chenjr15 <chenjr15@live.cn>
2019-08-30 12:18:58 +0800

    快慢指针找中点，递归建树

--- 109-convert-sorted-list-to-binary-search-tree/convert_sorted_list_to_binary_search_tree.go
+++ 109-convert-sorted-list-to-binary-search-tree/convert_sorted_list_to_binary_search_tree.go
@@ -1,8 +1,17 @@
 package leetcode109
 
-import "github.com/chenjr15/golist/linkedlist"
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
 
-type ListNode = linkedlist.ListNode
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
 
 func sortedListToBST(head *ListNode) *TreeNode {
 	// 要平衡就得保证左右节点数量一样多 ---> 根节点是中位数
@@ -10,4 +19,33 @@ func sortedListToBST(head *ListNode) *TreeNode {
 	// 可以找到中点，然后递归建左右子树， 这样问题就变成链表找中点和递归建树了
 	// 递归很简单，但问题也很明显，首先调用栈会比较深，然后链表找中点每次都得快慢指针遍历
 
+	if head == nil {
+		return nil
+	}
+	center, centerPre := findCenter(head)
+	var root *TreeNode = &TreeNode{
+		Val: center.Val,
+	}
+	if centerPre != nil {
+		centerPre.Next = nil
+		root.Left = sortedListToBST(head)
+	}
+	root.Right = sortedListToBST(center.Next)
+	return root
+
+}
+
+func findCenter(head *ListNode) (slow, slowPre *ListNode) {
+	if head == nil || head.Next == nil {
+		return head, slowPre
+	}
+	fast := head
+	slow = head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slowPre = slow
+		slow = slow.Next
+	}
+
+	return slow, slowPre
 }
