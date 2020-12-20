a5731877580a11561713ed9e40a52cc7decf3f3f
chenjr15 <chenjr15@live.cn>
2020-12-20 20:41:02 +0800

    添加递归解法

--- 116-populating-next-right-pointers-in-each-node/populating_next_right_pointers_in_each_node.go
+++ 116-populating-next-right-pointers-in-each-node/populating_next_right_pointers_in_each_node.go
@@ -1,1 +1,90 @@
 package leetcode116
+
+import "container/list"
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+// LevelOrderTraversal 层次遍历
+// 用队列实现，先把头节点入队， 然后取队头，访问队头，再把对头的左右孩子入队, 循环至队列空
+// level 从1 开始
+func LevelOrderTraversal(t *Node, visit func(*Node, int)) {
+	if t == nil {
+		return
+	}
+	level := 0
+	lastLevel := 0
+	queue := list.New()
+	if t != nil {
+		queue.PushBack(t)
+		lastLevel = 1
+	}
+
+	for lastLevel != 0 {
+		thisLevel := lastLevel
+		lastLevel = 0
+		for thisLevel != 0 {
+			t = (queue.Remove(queue.Front())).(*Node)
+			thisLevel--
+			if t.Left != nil {
+				queue.PushBack(t.Left)
+				lastLevel++
+			}
+			if t.Right != nil {
+				queue.PushBack(t.Right)
+				lastLevel++
+			}
+			visit(t, level)
+		}
+		level++
+
+	}
+
+}
+func connect(root *Node) *Node {
+	// 套以前的层序遍历模板
+	lastLevel := 0
+	var lastNode *Node
+	var visit = func(n *Node, level int) {
+		if lastNode != nil {
+			// fmt.Println(lastNode.Val,n.Val,level)
+			// 同一层内将上一个节点的Next指向当前节点
+			if lastLevel == level {
+				lastNode.Next = n
+			}
+		}
+		lastNode = n
+		lastLevel = level
+	}
+	LevelOrderTraversal(root, visit)
+	return root
+}
+
+var l, r *Node
+
+func visit(n *Node) {
+	if n == nil || n.Left == nil {
+		return
+	}
+	l = n.Left
+	r = n.Right
+	for l != nil {
+		l.Next = r
+		l = l.Right
+		r = r.Left
+	}
+	visit(n.Left)
+	visit(n.Right)
+
+}
+func connect(root *Node) *Node {
+
+	visit(root)
+	return root
+}
